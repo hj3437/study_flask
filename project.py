3f50961c47fece0e4198115ac898d64c64051bf0
HyukJin Kwon <insort@naver.com>
2021-08-31 19:22:39 +0900

    템플릿사용시 화면 배경색 변경

--- project.py
+++ project.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Store, Base, StoreItem
 
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///store.db', connect_args={'check_same_thread': False})
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 @app.route('/')
-def helloWorld():
-    return "Hello World!"
+def mainMenu():
+    stores = session.query(Store).all()
+    return render_template('store.html', sotres=stores)
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5151)
