f5df3fee2b2691fece19a85bb72ad71d6876f68c
HyukJin Kwon <insort@naver.com>
2021-08-31 15:25:35 +0900

    db 추가

---
+++ database_setup.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Store(Base):
+    __tablename__ = 'store'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class StoreItem(Base):
+    __tablename__ = 'store_item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(100))
+    price = Column(String(10))
+    package_item = Column(String(10))
+    store_id = Column(Integer, ForeignKey('store.id'))
+    store = relationship(Store)
+
+
+engine = create_engine('sqlite:///store.db')
+Base.metadata.create_all(engine)
